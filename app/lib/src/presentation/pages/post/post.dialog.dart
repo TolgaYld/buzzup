a6759a55e20464d2b9e1ba1598c92fac725ba463
Tolga Yildirim <tolga@ty-software.dev>
2025-01-28 00:26:49 +0100

    configure height of post dialog

--- app/lib/src/presentation/pages/post/post.dialog.dart
+++ app/lib/src/presentation/pages/post/post.dialog.dart
@@ -3,6 +3,7 @@ import 'package:buzzup/core/design/spacing.dart';
 import 'package:buzzup/core/hooks/use_l10n.hook.dart';
 import 'package:buzzup/core/hooks/use_theme.hook.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class PostDialog extends HookConsumerWidget with ShowableDialogMixin {
@@ -12,91 +13,115 @@ class PostDialog extends HookConsumerWidget with ShowableDialogMixin {
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = useTheme();
     final l10n = useL10n();
+    const minHeight = 0.4;
+    const maxHeight = 0.7;
+    final height = useState<double>(0.4);
+    final controller = useTextEditingController();
 
-    return Dialog(
-      insetPadding: EdgeInsets.zero,
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          return Container(
-            width: constraints.maxWidth,
-            height: constraints.maxHeight * 0.7,
-            padding: const EdgeInsets.all(Spacers.m),
-            decoration: BoxDecoration(
-              color: theme.colorScheme.background,
-              borderRadius: BorderRadius.circular(Spacers.s),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        TextField(
-                          minLines: 7,
-                          maxLines: null,
-                          decoration: InputDecoration(
-                            hintText: l10n.shareVib3Hint,
-                            border: OutlineInputBorder(),
+    useEffect(
+      () {
+        void updateHeight() {
+          if (height.value == 0.7) return;
+          final lines = (controller.text.split('\n').length).clamp(7, 21);
+          height.value = (lines * 0.045).clamp(minHeight, maxHeight);
+        }
+
+        controller.addListener(updateHeight);
+        return () => controller.removeListener(updateHeight);
+      },
+      [controller],
+    );
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Dialog(
+        insetPadding: EdgeInsets.zero,
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return AnimatedContainer(
+              duration: Durations.short3,
+              width: constraints.maxWidth,
+              height: constraints.maxHeight * height.value,
+              padding: const EdgeInsets.all(Spacers.m),
+              decoration: BoxDecoration(
+                color: theme.colorScheme.surface,
+                borderRadius: BorderRadius.circular(Spacers.s),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      primary: true,
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: controller,
+                            minLines: 7,
+                            maxLines: 30,
+                            decoration: InputDecoration(
+                              hintText: l10n.shareVib3Hint,
+                              border: OutlineInputBorder(),
+                            ),
                           ),
-                        ),
-                        const VSpace.s(),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            IconButton.filled(
-                              onPressed: () {},
-                              icon: Icon(Icons.camera_alt_rounded),
-                            ),
-                            const HSpace.s(),
-                            IconButton.filled(
-                              onPressed: () {},
-                              icon: Icon(Icons.image_rounded),
-                              style: IconButton.styleFrom(backgroundColor: theme.colorScheme.secondary),
-                            ),
-                            const HSpace.s(),
-                            IconButton.filled(
-                              onPressed: () {},
-                              icon: Icon(Icons.person_add_alt_rounded),
-                              style: IconButton.styleFrom(backgroundColor: Colors.green),
-                            ),
-                            const HSpace.s(),
-                            IconButton.filled(
-                              onPressed: () {},
-                              icon: Icon(Icons.pin_drop_rounded),
-                              style: IconButton.styleFrom(backgroundColor: Colors.lightBlue),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const VSpace.s(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () => Navigator.of(context).pop(),
-                      child: Text(l10n.close),
-                    ),
-                    const HSpace.s(),
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      label: Text(l10n.post),
-                      icon: Icon(
-                        Icons.send_rounded,
-                        color: theme.colorScheme.onPrimary,
+                          const VSpace.s(),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton.filled(
+                                onPressed: () {},
+                                icon: Icon(Icons.camera_alt_rounded),
+                              ),
+                              const HSpace.s(),
+                              IconButton.filled(
+                                onPressed: () => height.value = maxHeight,
+                                icon: Icon(Icons.image_rounded),
+                                style: IconButton.styleFrom(backgroundColor: theme.colorScheme.secondary),
+                              ),
+                              const HSpace.s(),
+                              IconButton.filled(
+                                onPressed: () {},
+                                icon: Icon(Icons.person_add_alt_rounded),
+                                style: IconButton.styleFrom(backgroundColor: Colors.green),
+                              ),
+                              const HSpace.s(),
+                              IconButton.filled(
+                                onPressed: () {},
+                                icon: Icon(Icons.pin_drop_rounded),
+                                style: IconButton.styleFrom(backgroundColor: Colors.lightBlue),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
-              ],
-            ),
-          );
-        },
+                  ),
+                  const VSpace.s(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: Text(l10n.close),
+                      ),
+                      const HSpace.s(),
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        label: Text(l10n.post),
+                        icon: Icon(
+                          Icons.send_rounded,
+                          color: theme.colorScheme.onPrimary,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
