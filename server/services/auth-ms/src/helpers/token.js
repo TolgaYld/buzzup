43b806da43260892dd87857cc2a2114b10ddc238
Tolga Yildirim <tolga@ty-software.dev>
2025-01-29 02:34:08 +0100

    add random id for every token

--- server/services/auth-ms/src/helpers/token.js
+++ server/services/auth-ms/src/helpers/token.js
@@ -1,11 +1,13 @@
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY, SECRET_KEY_REFRESH } = require("@TolgaYld/core-buzzup")
+const crypto = require("crypto");
 
 const token = {
   generate: ({ id }, expiresIn) => {
     return jwt.sign(
       {
         id,
+        jti: crypto.randomUUID(),
       },
       SECRET_KEY,
       { expiresIn },
@@ -18,6 +20,7 @@ const refreshToken = {
     return jwt.sign(
       {
         id,
+        jti: crypto.randomUUID(),
       },
       SECRET_KEY_REFRESH,
       { expiresIn },
